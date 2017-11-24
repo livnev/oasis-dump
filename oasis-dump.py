0df8465bf077e84fff00bd0e488b0aece207e688
reverendus <reverendus@invalid>
2017-11-24 15:00:47 +0100

    `--orders` implemented in `oasis-dump.py`

--- oasis-dump.py
+++ oasis-dump.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+#
 # This file is part of `oasis-dump`.
 #
 # Copyright (C) 2017 reverendus
@@ -17,7 +19,6 @@
 
 import argparse
 import json
-from pprint import pformat
 from typing import Optional, List
 
 import pkg_resources
@@ -35,22 +36,30 @@ class OfferInfo:
         self.owner = owner
         self.timestamp = timestamp
 
-    def __repr__(self):
-        return pformat(vars(self))
+    @staticmethod
+    def _format_amount(value: int):
+        tmp = str(value).zfill(19)
+        return (tmp[0:len(tmp)-18] + "." + tmp[len(tmp)-18:len(tmp)]).replace("-.", "-0.")
+
+    def __str__(self):
+        return f"Offer #{self.offer_id}: {self._format_amount(self.sell_how_much)} [{self.sell_which_token}]" \
+               f" for {self._format_amount(self.buy_how_much)} [{self.buy_which_token}] (owner: {self.owner})"
 
 
 parser = argparse.ArgumentParser(prog='oasis-dump')
 parser.add_argument("--rpc-host", help="JSON-RPC host (default: `localhost')", default="localhost", type=str)
 parser.add_argument("--rpc-port", help="JSON-RPC port (default: `8545')", default=8545, type=int)
-parser.add_argument("oasis-contract-address", help="Ethereum address of the OasisDEX smart contract")
+parser.add_argument("contract", help="Ethereum address of the OasisDEX smart contract", type=str)
+
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--orders',action='store_true',help='Compare two packages.')
+group.add_argument('--trades',action='store_true',help='Verify Content of package.')
+
 arguments = parser.parse_args()
 
 web3 = Web3(HTTPProvider(endpoint_uri=f"http://{arguments.rpc_host}:{arguments.rpc_port}"))
 abi = json.loads(pkg_resources.resource_string(__name__, 'SimpleMarket.abi'))
-contract = web3.eth.contract(abi=abi)(address=arguments.oasis_contract_address)
-
-last_order_id = contract.call().last_offer_id()
-
+contract = web3.eth.contract(abi=abi)(address=arguments.contract)
 
 def get_offer(offer_id: int) -> Optional[OfferInfo]:
     array = contract.call().offers(offer_id)
@@ -66,12 +75,15 @@ def get_offer(offer_id: int) -> Optional[OfferInfo]:
                          timestamp=array[6])
 
 
-def active_offers() -> List[OfferInfo]:
-    offers = [get_offer(offer_id + 1) for offer_id in range(last_order_id)]
-    return [offer for offer in offers if offer is not None]
+def print_orders():
+    offers = [get_offer(offer_id + 1) for offer_id in range(contract.call().last_offer_id())]
+    for offer in [offer for offer in offers if offer is not None]:
+        print(offer)
 
 
-offers = active_offers()
-print(offers)
+if arguments.orders:
+    print_orders()
 
+# if arguments.trades:
+#     print_trades()
 
